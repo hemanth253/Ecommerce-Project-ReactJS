411d0163276e3abbac553af6c91aa943f2618f13
hemanth253 <nhemanthreddy0@gmail.com>
2021-06-07 22:43:45 +0530

    done grouping items and quantity using cart utils

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toogleCartHidden = () => ({
   // payload is optional - check cartReducer for better undersstanding why
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
